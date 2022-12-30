2afd32a2816377556e37674b070085da9dac8ea7
zakb27 <zakbrook02@gmail.com>
2022-12-30 19:29:46 +0000

    Little bit of home added

--- firebase.js
+++ firebase.js
@@ -1,31 +1,5 @@
-// import * as firebase from "firebase";
-//
-// const firebaseConfig = {
-//     apiKey: "AIzaSyBTyXi7xD19kg3p-sqRHph-g_zUoSsoAKA",
-//     authDomain: "smart-home-81ebd.firebaseapp.com",
-//     projectId: "smart-home-81ebd",
-//     storageBucket: "smart-home-81ebd.appspot.com",
-//     messagingSenderId: "304145939648",
-//     appId: "1:304145939648:web:e4f63f7a0e160e070e8125",
-//     measurementId: "G-EDC747VQHF"
-// };
-//
-// let app;
-// if(firebase.apps.length===0){
-//     app = firebase.initializeApp(firebaseConfig);
-// }
-// else{
-//     app = firebase.app();
-// }
-//
-// const auth  = firebase.auth()
-//
-// export {auth}
-
-// import firebase from 'firebase/app';
-// import firebase from 'firebase/app';
 import { initializeApp } from 'firebase/app';
-import { getAuth } from "firebase/auth";
+import { initializeAuth } from "firebase/auth";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBTyXi7xD19kg3p-sqRHph-g_zUoSsoAKA",
@@ -38,6 +12,6 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
-const auth = getAuth(app);
+const auth = initializeAuth(app);
 
 export { auth };
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,16 +1,31 @@
-import React from 'react';
+import React, {useEffect,useState} from 'react';
 import {View, Text, Image, ScrollView, TextInput, SafeAreaView, TouchableOpacity,Button,StyleSheet} from 'react-native';
 import {auth} from "../../firebase";
 import {signOut} from "firebase/auth";
+import rooms from '../utils/rooms.json'
+import devices from '../utils/devices.json'
+
+
 const Home = ()=>{
-
-
+    const [currentRooms,setRooms] = useState('')
+    const [currentDevices,setDevices] = useState('')
+    useEffect(()=>{
+        setRooms(rooms.rooms)
+        setDevices(devices.devices)
+    },[])
 
     return(
         <SafeAreaView>
             <Text>
-                This is the Home page
+                You have {currentDevices.length} amount
             </Text>
+                {currentRooms.map((item,index)=>{
+                    return(
+                        <View key={index}>
+                            <Text>{item.name}</Text>
+                        </View>
+                    )
+                })}
 
         </SafeAreaView>
 
