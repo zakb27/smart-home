2a3cb4f750f10b9c384dc4b4557e6e4c129093b4
zakb27 <zakbrook02@gmail.com>
2023-01-26 20:23:38 +0000

    Tried setting up bluetooth and mqss

--- src/pages/Add.js
+++ src/pages/Add.js
@@ -1,12 +1,49 @@
-import React from 'react';
-import { View, Text, Image, ScrollView, TextInput,SafeAreaView } from 'react-native';
-
+import React,{useEffect} from 'react';
+import { View, Text, Image, ScrollView, TextInput,SafeAreaView,Button } from 'react-native';
+import BleManager from 'react-native-ble-plx';
+// const bleManager = new BleManager();
 const Add = ()=>{
+    const [connectionStatus, setConnectionStatus] = React.useState(false);
+    const [messages, setMessages] = React.useState([]);
+
+
+    // useEffect(() => {
+    //     
+    //     client.on('connect', () => {
+    //         setConnectionStatus(true)
+    //         client.subscribe('smarthome', function (err) {
+    //             if (!err) {
+    //                 client.publish('smarthome', 'Hello World');
+    //             }
+    //         })
+    //     });
+    //     client.on('message', (topic, payload, packet) => {
+    //         setMessages(messages.concat(payload.toString()));
+    //     });
+    //
+    // }, []);
+
+
+    const test = () =>{
+        console.log("SAOIMETUH")
+
+    }
+
     return(
+
+
+
         <SafeAreaView>
             <Text>
-                This is the add page
+                Temporary click with pi server
             </Text>
+
+            {/*<Button onPress={test} title={'Something'}>*/}
+            {/*    Connect*/}
+            {/*</Button>*/}
+
+            <Text>{messages}</Text>
+
 
         </SafeAreaView>
 
